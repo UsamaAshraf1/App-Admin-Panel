bb83a6eb4bb0e21626c7282e1197c12e7ef945e0
USAMA ASHRAF <usamaashraf728@gmail.com>
2025-07-29 16:22:59 +0500

    enhancement remove messaging

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,4 @@
-import { getMessaging, onMessage, getToken } from "firebase/messaging";
+// import {  onMessage, getToken } from "firebase/messaging";
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
@@ -12,40 +12,40 @@ const firebaseConfig = {
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
-const messaging = getMessaging(firebaseApp);
+// const messaging = getMessaging(firebaseApp);
 
 // Get registration token. Initially this makes a network call, once retrieved
 // subsequent calls to getToken will return from cache.
-export const fetchToken = (setTokenFound, setFcmToken) => {
-  return getToken(messaging, {
-    vapidKey:
-      "BKAD7SdrBmp9TIgQShG-k9T7NwxN9RRJNON3-wc_-4iqBpssKlkSDLtqeWt4exrDPUykX5RcrClIOYhewQtzJ_o",
-  })
-    .then((currentToken) => {
-      if (currentToken) {
-        // Send the token to your server and update the UI if necessary
-        setTokenFound(true);
-        setFcmToken(currentToken);
-      } else {
-        // Show permission request UI
-        console.log(
-          "No registration token available. Request permission to generate one."
-        );
-        setTokenFound(false);
-        setFcmToken("");
-      }
-    })
-    .catch((err) => {
-      console.log("An error occurred while retrieving token. ", err);
-      // ...
-    });
-};
+// export const fetchToken = (setTokenFound, setFcmToken) => {
+//   return getToken(messaging, {
+//     vapidKey:
+//       "BKAD7SdrBmp9TIgQShG-k9T7NwxN9RRJNON3-wc_-4iqBpssKlkSDLtqeWt4exrDPUykX5RcrClIOYhewQtzJ_o",
+//   })
+//     .then((currentToken) => {
+//       if (currentToken) {
+//         // Send the token to your server and update the UI if necessary
+//         setTokenFound(true);
+//         setFcmToken(currentToken);
+//       } else {
+//         // Show permission request UI
+//         console.log(
+//           "No registration token available. Request permission to generate one."
+//         );
+//         setTokenFound(false);
+//         setFcmToken("");
+//       }
+//     })
+//     .catch((err) => {
+//       console.log("An error occurred while retrieving token. ", err);
+//       // ...
+//     });
+// };
 
-export const onMessageListner = () => {
-  new Promise((resolve) => {
-    onMessage(messaging, (payload) => {
-      console.log("Message received. ", payload);
-      // ...
-    });
-  });
-};
+// export const onMessageListner = () => {
+//   new Promise((resolve) => {
+//     onMessage(messaging, (payload) => {
+//       console.log("Message received. ", payload);
+//       // ...
+//     });
+//   });
+// };
